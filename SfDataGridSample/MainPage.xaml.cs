01a76465c88213f6684168730a0083fd3074fd34
Nirmalkumar <nirmalkumar.yuvaraj@syncfusion.com>
2024-05-03 15:51:54 +0530

    Added sample and updated README.md

---
+++ SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Syncfusion.Maui.DataGrid;
+using System.Data;
+using System.Reflection;
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //dataGrid.AutoGeneratingColumn += DataGrid_AutoGeneratingColumn; ;
+        }
+
+        private void DataGrid_AutoGeneratingColumn(object? sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            if (e.Column.MappingName == "Salary")
+            {
+                e.Column = new DataGridTextColumn() { MappingName = "Salary", HeaderText = "Salary" };
+                e.Column.DisplayBinding = new Binding() { Path = "Salary", StringFormat = "{0:C}" };
+            }
+        }
+    } 
+}
